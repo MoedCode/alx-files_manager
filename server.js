b79288eb619d7b05497a6541f67b6eba9be6d86e
MoedCode <mohamed71291@gmail.com>
2024-06-18 16:38:54 +0300

    Tue Jun 18 16:38:54 EEST 2024

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import routes from './routes/index.js';
 // Express app
-const app  = express();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(routes);
 
-
-
-const server = app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
-})
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
