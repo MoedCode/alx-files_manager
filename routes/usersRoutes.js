9068345ac9c2adf1462106387a7a67745f20ff8a
MoedCode <mohamed71291@gmail.com>
2024-06-19 23:56:45 +0300

    Wed Jun 19 23:56:45 EEST 2024

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
 const usersRoutes = Router();
 
